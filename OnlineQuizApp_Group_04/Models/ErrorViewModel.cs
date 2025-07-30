3adf656ee112b9a98ef9c5dd810cd7e07ec8f316
kanhchakna hor <hor.kanhchakna.2822@rupp.edu.kh>
2025-07-30 18:36:13 +0700

    Initial commit of Online Quiz App project

---
+++ OnlineQuizApp_Group_04/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineQuizApp_Group_04.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
